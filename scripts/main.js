ff5224175af969be6931cb82679549d80b733427
A. James Lindsey <macbookair@MacBooks-MacBook-Air.local>
2020-09-09 14:07:46 -0400

    Completed Project

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,60 @@
-const $calculator = document.querySelector('.calculator');
-const $calculatorscreen = document.querySelector('.calculator-screen');
-const $calculatorkeys = document.querySelector('.calculator-keys');
-
-//const keys = $calculator.querySelector('.calculator-keys');
-
-/*keys.addEventListener('click', event => {
-  if (event.target.matches('button')) {
-  }
-});*/
+  //const $calculator = document.querySelector('.calculator');
+//sconst $calculatorkeys = document.querySelector('.calculator-keys');
 
 function pushNumber(event) {
-     alert(event.target.value);
+    (event.target.value);
+    calculation.push(event.target.value)
+    console.log(event.target.value);
   };
 
-const buttons = document.querySelectorAll('.number');
-  buttons.forEach(function(button) {
-     button.addEventListener('click', pushNumber);
+const number = document.querySelectorAll('.number');
+  number.forEach(function(button) {
+     button.addEventListener('click',pushNumber);
   });
 
-function pushOperator(event) {
-  alert(event.target.value);
+  function pushOperator(event) {
+    (event.target.value);
+  calculation.push(event.target.value)
+   console.log(event.target.value);
+  };
+  const operatorButtons = document.querySelectorAll('.operator');
+      operatorButtons.forEach(function(button) {
+         button.addEventListener('click',pushOperator);
+      });
+
+
+  const equalsign = document.querySelectorAll('.equal-sign');
+  equalsign.forEach(function(button){
+    button.addEventListener('click', calculate);
+
+  });
+
+const calculation = [];
+function calculate(event) {
+(event.target.value);
+calculation.push(event.target.value)
+
+let num1 = '';
+let num2 = '';
+let operator;
+let result;
+
+for (let i = 0; i < calculation.length; i++) {
+if (['+', '-', '*', '/'].includes (calculation[i])) {
+  operator = calculation[i];
+} else if (operator){
+  num2 = num2 + calculation [i];
+} else
+  num1 = num1 + calculation [i];
 }
+if (operator === '+') {
+result = parseFloat(num1) + parseFloat(num2);
+} else if (operator === '-'){
+result = parseFloat(num1) - parseFloat(num2);
+} else if (operator === '*'){
+result = parseFloat(num1) * parseFloat(num2);
+} else if (operator === '/'){
+result = parseFloat(num1) / parseFloat(num2);
 
-const operation = document.querySelectorAll('.operator');
-  operation.forEach(function(button){
-    button.addEventListener('click', pushOperator)
-  });
-
-  function pushEqualsign(event) {
-    alert(event.target.value);
-  }
-
-  const calculate = document. querySelectorAll('.equalsign');
-    calculate.forEach(function(button) {
-    button.addEventListener('click', pushEqualsign)
-  });
+} alert(result)
+};
